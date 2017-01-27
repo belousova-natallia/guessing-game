9d11f13bca47de92561fde1b27ddbc5beda1baf1
belousova-natallia <belousova_natallia@tut.by>
2017-01-28 00:04:39 +0300

    GuessingGame methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+    	this.number = number;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	var res = Math.ceil((this.min + this.max)/2)
+    	return res;
+    	
     }
 
     lower() {
+    	this.max = Math.ceil((this.min + this.max)/2);
 
     }
 
     greater() {
-
+	this.min = Math.ceil((this.min + this.max)/2)
     }
 }
 
 module.exports = GuessingGame;
+
